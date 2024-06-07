5cfd7d7ee8016f6974b29e8e676b52696a5b8b2e
Corbin Crutchley <git@crutchcorn.dev>
2024-06-06 20:25:46 -0700

    chore: add inject and external

---
+++ src/inject.ts
@@ -0,0 +1,15 @@
+import * as asar from "@electron/asar"
+
+const originalLinked = process._linkedBinding;
+
+// @ts-ignore
+process._linkedBinding = (...props) => {
+  const [name] = props;
+  if (name === "electron_common_asar") {
+    return asar;
+  }
+  return originalLinked(...props)
+};
+
+// @ts-ignore
+globalThis.__non_webpack_require__ = require

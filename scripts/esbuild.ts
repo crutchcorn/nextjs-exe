47731a4525b26bf9e195ca05fe5412423bbae178
Corbin Crutchley <git@crutchcorn.dev>
2024-06-07 03:42:26 -0700

    chore: WIP on resolve plugin

--- scripts/esbuild.ts
+++ scripts/esbuild.ts
@@ -18,5 +18,41 @@ build({
     path.resolve(__dirname, "../exe-src/inject-asar-fs.ts"),
     path.resolve(__dirname, "../exe-src/inject-asar-binding.ts")
   ],
+  plugins: [
+    {
+      name: "next-compiled-module-resolution",
+      setup(build) {
+        const resolvedSymbol = Symbol();
+        build.onResolve({filter: /./}, async (args) => {
+          if (args.kind !== "require-call") return;
+          if (args.pluginData === resolvedSymbol) return;
+
+          const resolved = await build.resolve(args.path, {
+            kind: args.kind,
+            importer: args.importer,
+            resolveDir: args.resolveDir,
+            pluginData: resolvedSymbol
+          })
+
+          if (resolved.errors.length > 0) {
+            const second = await build.resolve(args.path, {
+              kind: args.kind,
+              importer: args.importer,
+              resolveDir: path.resolve(__dirname, "../node_modules/next/dist/compiled"),
+              pluginData: resolvedSymbol
+            })
+
+            if (second.errors.length > 0) {
+              return {errors: resolved.errors}
+            }
+
+            return second;
+          }
+
+          return resolved;
+        })
+      }
+    }
+  ],
   entryPoints: [path.resolve(__dirname, "../exe-src/start.js")]
 })

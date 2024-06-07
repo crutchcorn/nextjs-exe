c7e9ca93a0a0c3873ce4d26e4b8c8268c9fc7cc0
Corbin Crutchley <git@crutchcorn.dev>
2024-06-06 21:32:15 -0700

    chore: update README

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ which could serve interestingly.
 ![](./assets/explainer.png)
 
 > This isn't a great visual explainer. I wrote it quickly [while live on Twitch](https://twitch.tv/crutchcorn).
+
+## Unfinished Warning
+
+This project is _mostly_ finished but is not tested, as we are blocked by this issue:
+
+https://github.com/nodejs/single-executable/issues/100
+
+### Will it work?
+
+I'm not sure, because our `fs` patch supposedly can read from `Buffer`s like `sea.getAsset` returns, however this **really heavily** expects
+Next.js to not introspect the `dir` path as if it were a string in any way outside of using the `fs` APIs. I'm doubtful this is the case, but I could be wrong.
--- exe-src/start.ts
+++ exe-src/start.ts
@@ -1,27 +1,23 @@
-// import 'next/dist/server/lib/cpu-profile'
-// import {startServer} from 'next/dist/server/lib/start-server'
-// import {printAndExit} from 'next/dist/server/lib/utils'
-// import {getProjectDir} from 'next/dist/lib/get-project-dir'
-// import {getReservedPortExplanation, isPortIsReserved,} from 'next/dist/lib/helpers/get-reserved-port'
-//
-// const dir = getProjectDir(directory)
-// const host = "locahost"
-// const port = 3000
-// let keepAliveTimeout = true
-//
-// if (isPortIsReserved(port)) {
-//     printAndExit(getReservedPortExplanation(port), 1)
-// }
-//
-// await startServer({
-//     dir,
-//     isDev: false,
-//     hostname: host,
-//     port,
-//     keepAliveTimeout,
-// })
-//
+import 'next/dist/server/lib/cpu-profile'
+import {startServer} from 'next/dist/server/lib/start-server'
+import {printAndExit} from 'next/dist/server/lib/utils'
+import {getReservedPortExplanation, isPortIsReserved,} from 'next/dist/lib/helpers/get-reserved-port'
 
-// TODO: Pass this to the Next server above and start server with patched `fs` module
-// import { getAsset } from 'node:sea';
-// const blob = getAsset('bundle.asar');
+const host = "locahost"
+const port = 3000
+let keepAliveTimeout = 300
+
+if (isPortIsReserved(port)) {
+    printAndExit(getReservedPortExplanation(port), 1)
+}
+
+import { getAsset } from 'node:sea';
+const dir = getAsset('bundle.asar') as never;
+
+await startServer({
+    dir,
+    isDev: false,
+    hostname: host,
+    port,
+    keepAliveTimeout,
+})
